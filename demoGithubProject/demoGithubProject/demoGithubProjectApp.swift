f59f67f6de4adbe4c78771ca601867f1878e3470
Theeratdolchat Chatchai <sitthach7777@gmail.com>
2024-06-23 10:06:17 +0700

    first commit

---
+++ demoGithubProject/demoGithubProject/demoGithubProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoGithubProjectApp.swift
+//  demoGithubProject
+//
+//  Created by Theeratdolchat Chatchai on 23/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct demoGithubProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
